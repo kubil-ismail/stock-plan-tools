2edff671925510a717aab73a5ea9819430a50ef9
kubil <bilkisismail07@gmail.com>
2026-04-20 14:19:46 +0700

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         inter.variable
       )}
     >
+      <Analytics />
       <body className="min-h-full flex flex-col">
         <div className="min-h-screen">
           {/* Header */}
